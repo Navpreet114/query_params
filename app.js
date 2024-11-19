a1edec1e11b44606d018d69b60906565cbd5bfe7
Navpreet114 <navpreet.singh@rdpolytech.ca>
2024-11-19 11:42:01 -0700

    first commit

--- app.js
+++ app.js
@@ -3,32 +3,34 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
+
+app.get("/", (req, res) =>{
     const id = req.query.id;
     const username = req.query.username;
 
-    res.send(`User id: ${id}. Username: ${username}`);
+    res.send(`User id: ${id}. Username is ${username}`);
 });
 
-app.get("/product/:id", (req, res) => {
+app.get("/products/:id", (req, res) =>{
 
-    //User supplied request parameter
+    //user supplied request parameter
     const product_id = req.params.id;
-    
-    //Product array or object
+
+    //products array or object
     const products = [
         {"id": 1, "name": "Product A"},
         {"id": 2, "name": "Product B"},
         {"id": 3, "name": "Product C"},
     ];
 
-    //Mapping functio to find product by id
-    const product = products.find(products => product.id === parseInt(product_id));
+   //mapping function to find product by id 
+   const product = products.find(products => products.id === parseInt(product_id));
 
-    //Use dot notation to access product fields and display
-    res.send(`Product ID: ${product.id}. Name: ${product.name}`);
+   //use dot notation to access product fields and display 
+   res.send(`Product ID: ${product.id}. Name: ${product.name}`);
 });
 
-app.listen(PORT, () => {
-    console.log(`Connected to port ${PORT}`);
+
+app.listen(PORT, ()=>{
+  console.log(`Connected to port ${PORT}`);
 });
